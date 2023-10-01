775d39c38df5121dbdb6cdba068b0edd8352217c
Wanshutri <valenzuelavivancorfelipe@gmail.com>
2023-10-01 16:58:03 -0300

    hacer responsivo skills, bugs arreglados y agregado skills y animacion

--- js/changeView.js
+++ js/changeView.js
@@ -1,10 +1,21 @@
 var selected = 'h';
 function alternateViews(view) {
     if (selected != view) {
+
+        var navItems = document.getElementsByClassName("nav-item");
+        for (var i = 0; i < navItems.length; i++) {
+            navItems[i].disabled = true;
+        }
+
         homeContent.classList.add('hidden');
         skillsContent.classList.add('hidden');
         proyectsContent.classList.add('hidden');
         cvContent.classList.add('hidden');
+
+        homeContent.classList.remove('fade-in');
+        skillsContent.classList.remove('fade-in');
+        proyectsContent.classList.remove('fade-in');
+        cvContent.classList.remove('fade-in');
 
         setTimeout(function () {
             homeContent.style.display = "none";
@@ -20,16 +31,23 @@ function alternateViews(view) {
             if (view === 'h') {
                 selected = 'h';
                 homeContent.style.display = "block";
+                homeContent.classList.add('fade-in');
             } else if (view === 's') {
                 selected = 's';
                 skillsContent.style.display = "block";
+                skillsContent.classList.add('fade-in');
             } else if (view === 'p') {
                 selected = 'p';
                 proyectsContent.style.display = "block";
+                proyectsContent.classList.add('fade-in');
             } else if (view === 'c') {
                 selected = 'c';
                 cvContent.style.display = "block";
+                cvContent.classList.add('fade-in');
             }
-        }, 500);
+            for (var i = 0; i < navItems.length; i++) {
+                navItems[i].disabled = false;
+            }
+        }, 600);
     }
 }
